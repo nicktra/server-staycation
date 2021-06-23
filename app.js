fc4c4a5a030e3c6a61820528464c0140cabf81c5
baloh <balohbaloh@yahoo.co.id>
2021-06-23 19:19:27 +0700

    Add view admin dashboard

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// import mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db_staycation', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// router admin
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -22,6 +32,8 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// admin
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
