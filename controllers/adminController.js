ab1133d2dcfd9bbc255121ca5599739da08c0a1b
baloh <balohbaloh@yahoo.co.id>
2021-07-02 20:25:27 +0700

    Add show image item

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -160,7 +160,7 @@ module.exports = {
   viewItem: async (req, res) => {
     try {
       const item = await Item.find()
-        .populate({ path: "imageId", select: "id imageURL" })
+        .populate({ path: "imageId", select: "id imageUrl" })
         .populate({ path: "categoryId", select: "id name" });
 
       const category = await Category.find();
@@ -171,7 +171,8 @@ module.exports = {
         title: "Staycation | Item",
         alert,
         category,
-        item
+        item,
+        action: "view",
       });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
@@ -206,6 +207,31 @@ module.exports = {
       req.flash("alertMessage", "Success Add Item");
       req.flash("alertStatus", "success");
       res.redirect("/admin/item");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/item");
+    }
+  },
+
+  showImageItem: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Item.findOne({ _id: id }).populate({
+        path: "imageId",
+        select: "id imageUrl",
+      });
+      console.log(item.imageId);
+
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render("admin/item/view_item", {
+        title: "Staycation | Show Image Item",
+        alert,
+        item,
+        action: "show image",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
