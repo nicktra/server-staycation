3009d5fec781f37d1af6429e24f1c04127e5f171
baloh <balohbaloh@yahoo.co.id>
2021-07-29 11:31:07 +0700

    Api Landing Page populate imageUrl

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,8 +4,10 @@ module.exports = {
   landingPage: async (req, res) => {
     try {
       const mostPicked = await Item.find()
-        .select("_id title country city price unit")
-        .limit(5);
+        .select("_id title country city price unit imageId")
+        .limit(5)
+        .populate({ path: 'imageId', select: '_id imageUrl' })
+        
       res.status(200).json({ mostPicked });
     } catch (error) {}
   },
