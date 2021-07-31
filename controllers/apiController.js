6d59aa5f27523ca5ec1d8b41e0530fab518e841c
baloh <balohbaloh@yahoo.co.id>
2021-07-31 12:46:58 +0700

    Api Landing Page category

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,7 @@
 const Item = require("../models/Item");
 const Treasure = require("../models/Activity");
 const Traveler = require("../models/Booking");
+const Category = require("../models/Category");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -8,22 +9,35 @@ module.exports = {
       const mostPicked = await Item.find()
         .select("_id title country city price unit imageId")
         .limit(5)
-        .populate({ path: 'imageId', select: '_id imageUrl' })
-        
-        const traveler = await Traveler.find();
-        const treasure = await Treasure.find();
-        const city = await Item.find();
+        .populate({ path: "imageId", select: "_id imageUrl" });
 
-      res.status(200).json({ 
-          hero: {
-              travelers: traveler.length,
-              treasures: treasure.length,
-              cities: city.length,
+      const category = await Category.find()
+        .select("_id name")
+        .limit(3)
+        .populate({
+          path: "itemId",
+          select: "_id title country city isPopular imageId",
+          perDocumentLimit: 4,
+          populate: {
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
           },
-          mostPicked 
         });
-    } catch (error) {
 
-    }
+      const traveler = await Traveler.find();
+      const treasure = await Treasure.find();
+      const city = await Item.find();
+
+      res.status(200).json({
+        hero: {
+          travelers: traveler.length,
+          treasures: treasure.length,
+          cities: city.length,
+        },
+        mostPicked,
+        category,
+      });
+    } catch (error) {}
   },
 };
