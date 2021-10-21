ce7b24cb6ca76ba23fa8a62701834dc7cef707cf
baloh <balohbaloh@yahoo.co.id>
2021-10-21 20:26:30 +0700

    Change maxAge session

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge: 60000 }
+  cookie: { maxAge: 600000 }
 }));
 app.use(flash());
 app.use(logger('dev'));
